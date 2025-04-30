582ce80ec5fa271d211238bf659a32ab4ee5589e
Sai Kranthi <iamsaikranthi@gmail.com>
2025-05-01 03:40:41 +0530

    feat: init cosmos adapters for payment and wallet

--- src/services/payment-adapters/index.ts
+++ src/services/payment-adapters/index.ts
@@ -5,9 +5,11 @@ import { EvmAdapter } from './EvmAdapter'
 import { PaymentAdapter } from './PaymentAdapter'
 import { SolanaAdapter } from './SolanaAdapter'
 import { WalletService } from '../../wallet/WalletService'
+import { CosmosAdapter } from './CosmosAdapter'
 
 export const ADAPTERS: Record<ChainType, new (wallet: WalletService) => PaymentAdapter> = {
   evm: EvmAdapter,
   arweave: ArweaveAdapter,
-  solana: SolanaAdapter
+  solana: SolanaAdapter,
+  cosmos: CosmosAdapter
 }
--- src/wallet/adapters/index.ts
+++ src/wallet/adapters/index.ts
@@ -5,9 +5,10 @@ import { EvmAdapter } from './EvmAdapter'
 import { WalletAdapter } from './WalletAdapter'
 import { Configuration } from '../Configuration'
 import { SolanaAdapter } from './SolanaAdapter'
-
+import { CosmosAdapter } from './CosmosAdapter'
 export const ADAPTERS: Record<ChainType, new (cfg: Configuration) => WalletAdapter> = {
   evm: EvmAdapter,
   arweave: ArweaveAdapter,
-  solana: SolanaAdapter
+  solana: SolanaAdapter,
+  cosmos: CosmosAdapter
 }
