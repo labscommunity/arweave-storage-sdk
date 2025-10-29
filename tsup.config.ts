06b8e60d470cf4379dcecfa2374545122a3373f2
Pawan Paudel <pawanpaudel93@gmail.com>
2025-10-29 23:05:04 +0545

    refactor: seperate config for node & browser

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,23 +2,35 @@ import type { Options } from 'tsup'
 
 const env = process.env.NODE_ENV
 
-export const tsup: Options = {
+const common: Options = {
   splitting: false,
   clean: true, // clean up the dist folder
-  dts: true, // generate dts files
-  format: ['cjs', 'esm', 'iife'], // generate cjs, iife and esm files
-  minify: env === 'production',
   bundle: true,
   skipNodeModulesBundle: true,
-  entryPoints: ['src/index.ts'],
   target: 'es2015',
   outDir: 'dist',
-  entry: ['src/index.ts'], // Only build the main entry point
   shims: true,
-  sourcemap: true,
-  tsconfig: './tsconfig.json',
-  external: [
-    '**/__tests__/**', // exclude test folders
-    '**/*.test.*' // exclude test files
-  ]
+  tsconfig: './tsconfig.json'
 }
+
+// Node.js configuration
+const node: Options = {
+  dts: true, // generate dts files
+  format: ['cjs', 'esm'], // Node.js and ES modules only
+  minify: env === 'production',
+  entry: ['src/**/*.ts', '!src/**/__tests__/**', '!src/**/*.test.*', '!src/mocks/**/*.ts'], //include all files under src
+  sourcemap: true,
+  ...common
+}
+
+// IIFE configuration
+const iife: Options = {
+  dts: false, // No types for IIFE
+  format: ['iife'], // Only IIFE format
+  minify: true,
+  entry: ['src/index.ts', '!src/**/__tests__/**', '!src/**/*.test.*', '!src/mocks/**/*.ts'], //include all files under src
+  sourcemap: false,
+  ...common
+}
+
+export default [node, iife]
