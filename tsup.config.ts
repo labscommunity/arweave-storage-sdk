533d76fb01a1ad6b598e241c5b0d542a9ec94606
Pawan Paudel <pawanpaudel93@gmail.com>
2025-10-29 21:32:01 +0545

    fix: update tsup configuration for development and watch modes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,14 @@
 import type { Options } from 'tsup'
 
-const env = process.env.NODE_ENV
+const isDev = process.env.NODE_ENV === 'development'
+const isWatch = process.argv.includes('--watch')
 
 export const tsup: Options = {
   splitting: false,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ['cjs', 'esm', 'iife'], // generate cjs, iife and esm files
-  minify: env === 'production',
+  minify: !isDev && !isWatch,
   bundle: true,
   skipNodeModulesBundle: true,
   entryPoints: ['src/index.ts'],
@@ -15,6 +16,6 @@ export const tsup: Options = {
   outDir: 'dist',
   entry: ['src/index.ts'], // Only build the main entry point
   shims: true,
-  sourcemap: env !== 'production', // Only generate source maps in development
+  sourcemap: isDev || isWatch, // Only generate source maps in development or watch mode
   tsconfig: './tsconfig.json'
 }
